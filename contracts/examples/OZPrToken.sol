ef35da960791e506a8c13968fb5d177c85a0a737
Matt Swezey <matt@mattswezey.net>
2018-10-04 02:00:41 -0500

    update OZ's PR Snapshot Token impl

--- contracts/examples/OZPrToken.sol
+++ contracts/examples/OZPrToken.sol
@@ -5,9 +5,12 @@
 pragma solidity 0.4.24;  
 
 import "../oz/ERC20Snapshot.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 
 /* solhint-disable */
-contract OzPrToken is ERC20Snapshot {
+contract OzPrToken is ERC20Snapshot, ERC20Pausable, ERC20Burnable, ERC20Mintable {
     string public constant name = "OZ PR TOKEN";
     string public constant symbol = "OZPR";
     uint8 public constant decimals = 18;
