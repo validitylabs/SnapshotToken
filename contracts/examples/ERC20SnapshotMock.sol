1571c88fe68ca05fecbbf383b6eb9ad70ea67e62
Matt Swezey <matt@mattswezey.net>
2018-10-08 12:16:53 -0500

    Revert "Revert "Add more snapshot tests throughout successful transfer events""
    This reverts commit b4ab2bd6696d31c523119e8200b493d288dd8918.

--- contracts/examples/ERC20SnapshotMock.sol
+++ contracts/examples/ERC20SnapshotMock.sol
@@ -5,19 +5,19 @@ import "../token/ERC20/ERC20Snapshot.sol";
 
 // mock class using ERC20Snapshot
 contract ERC20SnapshotMock is ERC20Snapshot {
-  constructor(address initialAccount, uint256 initialBalance) public {
-    _mint(initialAccount, initialBalance);
-  }
+    constructor(address initialAccount, uint256 initialBalance) public {
+        _mint(initialAccount, initialBalance);
+    }
 
-  function mint(address account, uint256 amount) public {
-    _mint(account, amount);
-  }
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
 
-  function burn(address account, uint256 amount) public {
-    _burn(account, amount);
-  }
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
 
-  function burnFrom(address account, uint256 amount) public {
-    _burnFrom(account, amount);
-  }
+    function burnFrom(address account, uint256 amount) public {
+        _burnFrom(account, amount);
+    }
 }
